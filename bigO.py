b92e4814968bbbff407009e8c666c9b2edec11ae
shaha <shaha0111@mail.ru>
2022-04-30 01:45:28 +0600

    bigO

---
+++ bigO.py
@@ -0,0 +1,23 @@
+"""
+bigO - to compare two solutions
+Time comlpexity - it is measured in the number of operations performed
+Space comlpexity - measured by size of memory space is taken for operations
+
+5. BigO: worst case
+O - omicron (algorithm and data structure efficiency in worst case) like lim->∞
+
+7. BigO: drop constants
+For O(2n) - we drop constants so O(2n)->O(n) so the constant value doesn't matter
+
+8. BigO: O(n^2)
+Example for O(n^2) - when we have 1 for loop inside another. O(n^3)->O(n^2)
+
+9. BigO: drop non-dominants
+O(n^2) + O(n) -> O(n^2 + n) -> O(n^2)
+
+10. BigO: O(1) - constant time - most efficient
+
+11. BigO: O(log(n))
+
+12. BigO: different terms for inputs
+"""
